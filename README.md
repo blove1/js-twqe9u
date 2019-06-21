4f18b141a6b022291ec9516798320e6535ccb30a
blove1 <binhth3980@gmail.com>
2019-06-21 11:35:32 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-twqe9u
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-twqe9u)
---
+++ index.js
@@ -0,0 +1,27 @@
+// Import stylesheets
+import '@grapecity/wijmo.styles/wijmo.css';
+import './style.css';
+
+import "@grapecity/wijmo";
+import "@grapecity/wijmo.input";
+import "@grapecity/wijmo.grid";
+import "@grapecity/wijmo.viewer";
+
+// Write Javascript code!
+const viewer = new wijmo.viewer.ReportViewer('#viewer', {
+  serviceUrl: 'https://demos.componentone.com/ASPNET/c1webapi/4.0.20172.105/api/report',
+  filePath: 'ReportsRoot/Formatting/AlternateBackground.flxr',
+  reportName: 'AlternateBackground',
+  refreshed: function (sender) {
+    //let exportBtn = sender.hostElement.querySelector('.wj-exportformats-wrapper .wj-applybutton');
+     //exportBtn.style.display = 'none';
+
+    var exportBtn = sender.hostElement.querySelector('a[class~="wj-btn"][title="Export"][command-tag="28"]');
+    var newExportBtn = exportBtn.cloneNode(true);
+    exportBtn.parentNode.replaceChild(newExportBtn, exportBtn);
+
+    newExportBtn.addEventListener("click", function(e) {
+      alert("Do your own export here");
+    });
+  }
+});
